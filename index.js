8a0a9682f37b52da5e63cde0fb01a0a8ab9e357e
João Palmeiro <joaommpalmeiro@gmail.com>
2021-07-11 15:18:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,4 +7,13 @@ const log = console.log;
 // Source:
 // - https://github.com/tj/commander.js/issues/48#issuecomment-49087390
 // - https://stackoverflow.com/a/10855054
-program.version(pjson.version).description(pjson.description).parse();
+program.version(pjson.version).description(pjson.description);
+
+program
+  .option('-l, --length <number>', 'password length (number of characters)', '8')
+  .option('-nn, --no-numbers', 'remove numbers')
+  .option('-ns, --no-symbols', 'remove symbols')
+  .parse();
+
+// console.log(program.opts());
+const { length, numbers, symbols } = program.opts();
