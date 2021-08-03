a03e696c4ecfee3374fb74654477b330c345b83a
João Palmeiro <joaommpalmeiro@gmail.com>
2021-08-03 14:34:14 +0100

    Update createPassword.js

--- utils/createPassword.js
+++ utils/createPassword.js
@@ -1,3 +1,5 @@
+const crypto = require('crypto');
+
 const alpha = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const numbers = '0123456789';
 const symbols = '!@#$%^&*_-+=';
@@ -5,10 +7,33 @@ const symbols = '!@#$%^&*_-+=';
 const createPassword = (length = 8, hasNumbers = true, hasSymbols = true) => {
   let chars = alpha;
 
-  hasNumbers ? (chars += numbers) : '';
-  hasSymbols ? (chars += symbols) : '';
+  // hasNumbers ? (chars += numbers) : '';
+  // hasSymbols ? (chars += symbols) : '';
+  // or
+  hasNumbers && (chars += numbers);
+  hasSymbols && (chars += symbols);
 
-  return chars;
+  return generatePassword(length, chars);
+};
+
+const generatePassword = (length, chars) => {
+  const random = crypto.randomBytes(Number(length));
+  console.log(random[7] % chars.length);
+  let password = '';
+
+  for (let i = 0; i < length; i++) {
+    // `Math.random()` does not provide cryptographically secure random numbers.
+    // More info:
+    // - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+    // - https://github.com/bradtraversy/passgen/issues/2
+    // - https://github.com/bradtraversy/passgen/pull/3
+    // - https://nodejs.org/docs/latest-v14.x/api/crypto.html#crypto_crypto_randombytes_size_callback
+    // password += chars.charAt(Math.floor(Math.random() * chars.length));
+    // `%`: to get array values with wrapping.
+    password += chars.charAt(random[i] % chars.length);
+  }
+
+  return password;
 };
 
 module.exports = createPassword;
